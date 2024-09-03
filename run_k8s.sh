673fafbf30ba6d51dd224505b39d4f9cd4375226
LucasAbbiati <lucasabbiati.p@gmail.com>
2024-09-02 23:55:22 -0300

    feat: subindo a aplicaçao com Kubernets

---
+++ run_k8s.sh
@@ -0,0 +1,5 @@
+kubectl apply -f k8s/backend-pvc.yaml
+kubectl apply -f k8s/backend-deployment.yaml
+kubectl apply -f k8s/frontend-deployment.yaml
+kubectl apply -f k8s/backend-service.yaml
+kubectl apply -f k8s/backend-service.yaml
